21b761e66bd45d635c13b115f7b5fcf2b13855d4
Radek Stepan <dev@radekstepan.com>
2026-01-09 19:53:13 -0800

    chore: wipe

--- src/delay.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A simple promise-based delay function.
- * @param ms - The number of milliseconds to wait.
- */
-export function delay(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
